8abd4200e5b6c3ba73d15318d6121a5211bfd357
Jose Mario Lami <jmlami99@gmail.com>
2019-04-25 21:22:33 -0400

    Commit con MVC completo

---
+++ Tarea/MVCTarea/Models/DataContextS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCTarea.Models
+{
+    public class DataContextS : DbContext
+    {
+        public DataContextS() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCTarea.Models.Sale> Sales { get; set; }
+    }
+}
