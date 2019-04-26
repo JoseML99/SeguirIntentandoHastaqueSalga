0503035aa92748bebfeea1a227eb4c1803ff002c
Jose Mario Lami <jmlami99@gmail.com>
2019-04-25 21:17:31 -0400

    Commit con modelo de producto y clientes MVC

---
+++ Tarea/MVCTarea/Models/DataContextC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCTarea.Models
+{
+    public class DataContextC : DbContext
+    {
+        public DataContextC() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCTarea.Models.Client> Clients { get; set; }
+    }
+}
---
+++ Tarea/MVCTarea/Models/DataContextP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCTarea.Models
+{
+    public class DataContextP:DbContext
+    {
+        public DataContextP():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCTarea.Models.Product> Products { get; set; }
+    }
+}
